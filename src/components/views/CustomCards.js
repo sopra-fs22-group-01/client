b597e1467b8b5dd0874887aa5eb1e35c41751802
thphan21 <thuan.phan@uzh.ch>
2022-05-10 01:08:13 +0200

    created custom cards page and css, not done

---
+++ src/components/views/CustomCards.js
@@ -0,0 +1,110 @@
+import React, {useState, Component} from 'react';
+import {api, handleError} from 'helpers/api';
+import User from 'models/User';
+import {Link, useHistory} from 'react-router-dom';
+import {PrimaryButton} from 'components/ui/PrimaryButton';
+import 'styles/views/CustomCards.scss';
+import BaseContainer from "components/ui/BaseContainer";
+import PropTypes from "prop-types";
+import {AiOutlineCheckCircle} from "react-icons/ai";
+import {CardButton} from "../ui/CardButton";
+import Card from "../../models/Card";
+
+
+/*
+It is possible to add multiple components inside a single file,
+however be sure not to clutter your files with an endless amount!
+As a rule of thumb, use one file per component and only add small,
+specific components that belong to the main one in the same file.
+ */
+const FormField = props => {
+    return (
+        <div className="customCards field">
+            <label className="customCards label">
+                {props.label}
+            </label>
+            <input
+                className="customCards input"
+                value={props.value}
+                onChange={e => props.onChange(e.target.value)}
+            />
+
+        </div>
+    );
+};
+
+FormField.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func
+};
+
+const CustomCards = props => {
+   //const current = new Date();
+   // const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    const history = useHistory();
+    const [password, setPassword] = useState(null);
+    const [username, setUsername] = useState(null);
+
+    //in what order does the "Registration" run?
+
+    const doRegistration = async () => {
+        try {
+            const requestBody = JSON.stringify({username, password});
+            const response = await api.post('/users', requestBody); //request get to restcontoller (POST sends to server)
+            /*await makes sure doRegistration waits until it gets a response before it goes on.
+              however, other functions in the program can already run. so the return, where the username and birthdate get asked
+              can already be executed(?)
+                * */
+            // Get the returned user and update a new object.
+            const user = new User(response.data);
+
+            // Store the token into the local storage.
+            localStorage.setItem('token', user.token);
+
+            // Login successfully worked --> navigate to the route /game in the GameRouter
+            history.push(`/users/profile/${user.id}`);
+        } catch (error) {
+            alert(`Something went wrong during the registration: \n${handleError(error)}`);
+        }
+    };
+
+    return (
+        <BaseContainer>
+            <div className="customCards container">
+                <h1>Create your own custom card</h1>
+                <div className="customCards form">
+                    <FormField
+                        label="custom text:"
+                        value={username}
+                        onChange={un => setUsername(un)}
+                    />
+
+                    <div className="customCards button-container">
+                        <PrimaryButton
+                            disabled={!username} //if no birthday or username is entered, button cant be clicked
+                            width="50%" //define size of button here so it can be 100% in the button template
+                            onClick={() => doRegistration()}
+                        >
+                            Register
+                        </PrimaryButton>
+                    </div>
+                    <div className="customCards registrationText">
+                        <text>Already have an account? Click </text>
+                        <Link to={`/users/login`}>
+                            <text>here</text>
+                        </Link>
+                        <text> to log in.</text>
+                    </div>
+                </div>
+
+            </div>
+        </BaseContainer>
+    );
+};
+
+/**
+ * You can get access to the history object's properties via the withRouter.
+ * withRouter will pass updated match, location, and history props to the wrapped component whenever it renders.
+ */
+export default CustomCards;
