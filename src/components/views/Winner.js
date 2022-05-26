b46e5d0aba54c39c6d41e33aabc4110142b44c9c
lsenni <larissa.senning@uzh.ch>
2022-05-26 17:53:37 +0200

    Setted timeout

--- src/components/views/Winner.js
+++ src/components/views/Winner.js
@@ -195,14 +195,17 @@ const Winner = () => {
         return text;
     }
     //blackCard && scores && !read
-    if (!read){
-        var synth = window.speechSynthesis;
-        let utter = new SpeechSynthesisUtterance();
-        utter.lang = 'en-US';
-        utter.text = textToRead();
-        synth.speak(utter);
-        setRead(true);
+    function textToSpeech(){
+        if (!read){
+            var synth = window.speechSynthesis;
+            let utter = new SpeechSynthesisUtterance();
+            utter.lang = 'en-US';
+            utter.text = textToRead();
+            synth.speak(utter);
+            setRead(true);
+        }
     }
+    setTimeout(textToSpeech, 1000);
 
     return (
         <BaseContainer className="round container">
